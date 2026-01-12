2ad7fb966ff03d532477811220aacc3045e091c9
ssanmathimadhyastha-arch <ssanmathimadhyastha@gmail.com>
2026-01-12 20:59:12 +0530

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true },
+  password: String,
+  role: { type: String, default: "user" }
+});
+
+UserSchema.pre("save", async function () {
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+module.exports = mongoose.model("User", UserSchema);
